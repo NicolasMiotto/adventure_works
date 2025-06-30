96eff5e1c2fe60111b13dd2efd78e125dc38050b
Nicolas <nicolasmiotto3@gmail.com>
2025-06-29 22:05:42 -0300

    adicionando testes e documentação

--- tests/test_product_value_2011.sql
+++ tests/test_product_value_2011.sql
@@ -1,12 +1,12 @@
 with
-    tst_total_product_value_2011 as (
+    test_sales_2011 as (
         select
-            sum(product_real_value) as product_real_value
+            sum(product_real_value) as product_value
         from {{ ref('int_sales') }}
         where duedate between '2011-01-01' and '2011-12-31'
     )
     
 select
-    product_real_value
-from tst_total_product_value_2011
-where product_real_value between 12646086 and 12647006
+    product_value
+from test_sales_2011
+where product_real_value between 12646112.05 and 12646112.30
